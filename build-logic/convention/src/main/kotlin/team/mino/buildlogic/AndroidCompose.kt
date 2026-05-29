db3ad58fd65cfebe889902eca5d7398b78e3b4b7
Jaesung Lee <viki0114@naver.com>
2026-05-29 15:54:56 +0900

    chore: Slack Compose Lint 도입
    Compose 컨벤션 플러그인에 lintChecks 의존성과 lint.xml 연결을 추가해
    Compose가 적용된 모든 모듈에 룰을 자동 전파한다. 도입 과정에서 검출된
    GreetingPreview의 ComposePreviewPublic 위반(public preview)도 함께 수정.

--- build-logic/convention/src/main/kotlin/team/mino/buildlogic/AndroidCompose.kt
+++ build-logic/convention/src/main/kotlin/team/mino/buildlogic/AndroidCompose.kt
@@ -9,11 +9,13 @@ import org.gradle.kotlin.dsl.dependencies
 // CommonExtension 단일 시그니처로 합칠 수 없음.
 internal fun Project.configureAndroidCompose(extension: ApplicationExtension) {
     extension.buildFeatures.compose = true
+    extension.lint.lintConfig = rootProject.file("lint.xml")
     addComposeDependencies()
 }
 
 internal fun Project.configureAndroidCompose(extension: LibraryExtension) {
     extension.buildFeatures.compose = true
+    extension.lint.lintConfig = rootProject.file("lint.xml")
     addComposeDependencies()
 }
 
@@ -32,5 +34,7 @@ private fun Project.addComposeDependencies() {
         add("debugImplementation", libs.lib("androidx-compose-ui-test-manifest"))
 
         add("androidTestImplementation", libs.lib("androidx-compose-ui-test-junit4"))
+
+        add("lintChecks", libs.lib("slack-compose-lints"))
     }
 }
