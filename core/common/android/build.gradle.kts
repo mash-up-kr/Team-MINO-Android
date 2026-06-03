cfdef02730b778fb398f473a4c65770f07fa80e8
Chea-yunzi <katete051@naver.com>
2026-06-03 17:14:32 +0900

    refactor: `core:common:android` 모듈의 코루틴 의존성 노출 범위 수정
    `kotlinx-coroutines-android` 의존성을 `api`에서 `implementation`으로 변경하여 불필요한 의존성 전파를 제한함.

--- core/common/android/build.gradle.kts
+++ core/common/android/build.gradle.kts
@@ -10,6 +10,6 @@ dependencies {
     implementation(project(":core:common:kotlin"))
 
     implementation(libs.androidx.core.ktx)
-    api(libs.kotlinx.coroutines.android)
+    implementation(libs.kotlinx.coroutines.android)
     implementation(libs.timber)
 }
