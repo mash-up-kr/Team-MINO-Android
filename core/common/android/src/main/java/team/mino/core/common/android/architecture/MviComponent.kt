622e53a384cc30bace0304228b4442013764b3fa
Chea-yunzi <katete051@naver.com>
2026-06-03 15:03:06 +0900

    refactor: MviContainer 인터페이스 및 관련 팩토리 함수 제거
    `MviComponent.kt`에서 `MviContainer` 인터페이스와 `mviContainer` 팩토리
    함수 정의를 삭제함. 해당 파일에는 `UiState`, `UiIntent`, `UiSideEffect`
    마커 인터페이스만 남겨 역할을 단순화함.

--- core/common/android/src/main/java/team/mino/core/common/android/architecture/MviComponent.kt
+++ core/common/android/src/main/java/team/mino/core/common/android/architecture/MviComponent.kt
@@ -1,31 +1,10 @@
 package team.mino.core.common.android.architecture
 
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.StateFlow
-
 /**
  *  MVI 기본 인터페이스
  */
 interface UiState
+
 interface UiIntent
+
 interface UiSideEffect
-
-/**
- * ViewModel이 상속받아 사용할 MVI 인터페이스
- */
-interface MviContainer<S : UiState, E : UiSideEffect> {
-    val state: StateFlow<S>
-    val sideEffect: Flow<E>
-
-    fun updateState(reducer: S.() -> S)
-    suspend fun postSideEffect(effect: E)
-}
-
-/**
- * ViewModel 내부에서 MviContainer 구현체를 편리하게 조립하기 위한 함수입니다.
- *
- * @param initialState 화면의 초기 [UiState]
- */
-fun <S : UiState, E : UiSideEffect> mviContainer(
-    initialState: S
-): MviContainer<S, E> = MviContainerImpl(initialState)
