1e2111eaefe7e8db2510ac6b95f28259f2e25e16
Jaesung Lee <viki0114@naver.com>
2026-06-10 17:11:56 +0900

    refactor: Home impl 화면-단위 패키지 구조 재편
    sample과 동일한 화면 단위(main/di) 패키지 구조로 home impl을 옮긴다.
    HomeNavigation을 HomeDestinations와 HomeNavHost로 분리하고, Launcher
    구현체를 di로, 화면을 main/screen으로 이동한다. 동작 변화는 없다.

--- feature/home/impl/src/main/java/team/mino/feature/home/HomeActivity.kt
+++ feature/home/impl/src/main/java/team/mino/feature/home/HomeActivity.kt
@@ -13,7 +13,6 @@ import dagger.hilt.android.AndroidEntryPoint
 import team.mino.core.designsystem.theme.MinoAndroidAppTheme
 import team.mino.feature.home.api.EXTRA_HOME_GREETING
 import team.mino.feature.home.api.EXTRA_HOME_RESULT_CONFIRMED
-import team.mino.feature.home.navigation.HomeNavHost
 import team.mino.feature.sample.api.EXTRA_FROM_HOME
 import team.mino.feature.sample.api.SampleLauncher
 import javax.inject.Inject
---
+++ feature/home/impl/src/main/java/team/mino/feature/home/HomeDestinations.kt
@@ -0,0 +1,7 @@
+package team.mino.feature.home
+
+import kotlinx.serialization.Serializable
+import team.mino.core.navigation.screen.Route
+
+@Serializable
+internal data object HomeMain : Route
