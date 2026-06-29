eddc2c57f5d95491393d3fb8182883aa409051d6
full_avocado <ses8323@gmail.com>
2026-06-29 23:36:18 +0900

    refactor: Github 네트워크 레이어를 Ktorfit에서 Ktor로 전환
    Ktorfit의 create<T>()가 컴파일러 플러그인에 의존해 구현체를 연결하지
    못하면서 런타임 ClassCastException이 발생했다. Ktorfit 플러그인·의존성을
    제거하고 Ktor HttpClient를 직접 호출하는 방식으로 GithubApiService를
    재구성했다. baseUrl은 HttpClient의 defaultRequest로 옮겼다.

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,6 @@ plugins {
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.hilt) apply false
-    alias(libs.plugins.ktorfit) apply false
     alias(libs.plugins.ktlint) apply false
     alias(libs.plugins.google.services) apply false
     alias(libs.plugins.firebase.crashlytics) apply false
