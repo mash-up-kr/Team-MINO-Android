ee244addd2f47187f2310385bc5093056bc8d5d3
Jaesung Lee <viki0114@naver.com>
2026-07-24 17:39:31 +0900

    feat: 시스템 폰트 배율 무시하고 텍스트 크기 고정
    MinoAndroidAppTheme에서 LocalDensity의 fontScale을 1f로 오버라이드해
    테마 하위 텍스트가 항상 1sp = 1dp로 렌더링되도록 함. 시스템 폰트
    크기 설정이 커져도 디자인 기준 레이아웃이 깨지지 않게 하려는 결정
    (ADR 0003). 검증용 fontScale 2배 프리뷰 추가.

--- core/design-system/src/main/java/team/mino/core/designsystem/foundation/typography/TypographyPreview.kt
+++ core/design-system/src/main/java/team/mino/core/designsystem/foundation/typography/TypographyPreview.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import team.mino.core.designsystem.foundation.color.token.ColorAccessKeyToken
 import team.mino.core.designsystem.foundation.color.token.value
@@ -16,6 +17,8 @@ import team.mino.core.designsystem.theme.MinoAndroidAppTheme
 import team.mino.core.designsystem.util.preview.UiModePreviews
 
 @UiModePreviews
+// 시스템 폰트 배율이 커져도 텍스트 크기가 고정(1sp = 1dp)되어 UiMode 프리뷰와 동일해야 한다
+@Preview(fontScale = 2f, name = "FontScale2x")
 @Composable
 private fun TypographyPreview() {
     MinoAndroidAppTheme {
