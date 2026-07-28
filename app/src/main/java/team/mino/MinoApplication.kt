6d248c49a38edb7903219cd371c1d8d4eca68b50
Jaesung Lee <viki0114@naver.com>
2026-07-28 16:48:34 +0900

    feat: 빌드 타입별 Timber 트리 추가
    debug는 파일명·줄번호 태그의 FileLineDebugTree로 Logcat에,
    release는 CrashlyticsTree로 WARN 이상만 브레드크럼·non-fatal로
    Crashlytics에 기록한다. 코드 레벨은 Timber 파사드만 사용한다.

--- app/src/main/java/team/mino/MinoApplication.kt
+++ app/src/main/java/team/mino/MinoApplication.kt
@@ -2,14 +2,22 @@ package team.mino
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import team.mino.logging.CrashlyticsTree
+import team.mino.logging.FileLineDebugTree
 import timber.log.Timber
 
 @HiltAndroidApp
 class MinoApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        initializeLogger()
+    }
+
+    private fun initializeLogger() {
         if (BuildConfig.DEBUG) {
-            Timber.plant(Timber.DebugTree())
+            Timber.plant(FileLineDebugTree())
+        } else {
+            Timber.plant(CrashlyticsTree())
         }
     }
 }
