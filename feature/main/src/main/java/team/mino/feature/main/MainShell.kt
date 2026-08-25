2408253e59cb1032a6886126fa62e1aaaed640c3
Jaesung Lee <viki0114@naver.com>
2026-08-25 21:37:30 +0900

    feat: 방 폼 임시 검증 진입점 추가
    폼을 여는 경로가 저장소 어디에도 없어 손으로 눌러 볼 수단을 임시로 둔다. 실제 진입점
    feature가 생기면 이 진입점과 그 타입을 관통시키는 파라미터를 함께 걷어낸다.

--- feature/main/src/main/java/team/mino/feature/main/MainShell.kt
+++ feature/main/src/main/java/team/mino/feature/main/MainShell.kt
@@ -7,11 +7,13 @@ import androidx.navigation.compose.rememberNavController
 import team.mino.core.analytics.screen.TrackScreenViews
 import team.mino.core.common.ui.scaffold.MinoScaffold
 import team.mino.feature.main.component.MainBottomBar
+import team.mino.feature.main.placeholder.RoomFormEntryPoint
 
 @Composable
 internal fun MainShell(
     onNavigateToSample: () -> Unit,
     onRequestSampleResult: () -> Unit,
+    roomFormEntryPoint: RoomFormEntryPoint,
     modifier: Modifier = Modifier,
 ) {
     val navController = rememberNavController()
@@ -31,6 +33,7 @@ internal fun MainShell(
             navController = navController,
             onNavigateToSample = onNavigateToSample,
             onRequestSampleResult = onRequestSampleResult,
+            roomFormEntryPoint = roomFormEntryPoint,
             modifier = Modifier.padding(innerPadding),
         )
     }
